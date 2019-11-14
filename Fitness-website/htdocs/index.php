bc4d443bb50c173358a3be81723b1004a37b0971
murtadello <55238439+murtadello@users.noreply.github.com>
2019-11-14 09:44:35 +0100

    added

--- Fitness-website/htdocs/index.php
+++ Fitness-website/htdocs/index.php
@@ -1,8 +1,12 @@
-<?php include "header.php" ?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 <?php
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(E_ALL);
 
 /*
 define(Name, "Murtaza");
@@ -49,94 +53,37 @@ $woman = array(
 ?>
 */
 
-// for($i=0;$i<10;$i++){
-//     echo "Hello World!" ."<br>";
-// }
-// $i=0;
-// while($i<=10){
-//     echo "while loop". "<br>";
-//     $i++;
-// }
+for($i=0;$i<10;$i++){
+    echo "Hello World!" ."<br>";
+}
+$i=0;
+while($i<=10){
+    echo "while loop". "<br>";
+    $i++;
+}
 
 
-// $today="Wednes";
-// switch($today){
-//     case "Mon":
-//     echo "today is monday";
-//     break;
-//     case "Tues":
-//     echo "Today is Tuesday";
-//     break;
-//     case "Wednes":
-//     echo "Today is wednesday";
-//     break;
-//     case "thurs":
-//     echo "today is thursday";
-//     break;
-//     default:
-//     echo "invalid day";
-//     break;
+$today="Wednes";
+switch($today){
+    case "Mon":
+    echo "today is monday";
+    break;
+    case "Tues":
+    echo "Today is Tuesday";
+    break;
+    case "Wednes":
+    echo "Today is wednesday";
+    break;
+    case "thurs":
+    echo "today is thursday";
+    break;
+    default:
+    echo "invalid day";
+    break;
 
-// }
-// $h=234.56;
-// $int_cast=(int)$h;
-// echo $int_cast;
-// echo "<br>";
-
-
-// define("GREETING", "Welcome to w3school.com");
-// echo "<h1>" .GREETING ."</h1>";
-
-
-// define("cars", [
-//     "Alfa Romeo",
-//     "BMW",
-//     "Toyota"
-// ]);
-
-
-// for($i=0;$i<sizeof(cars); $i++){
-//     echo cars[$i]. "<br>";
-// }
-
-// define("MESSAGE", "Welcome to w3school.com");
-// function myTest(){
-//     echo "<h1>" .MESSAGE ."</h1>";
-// }
-// myTest();
-
-
-// $t =Date("H");
-// if($t<"10"){
-//     echo "have a good morning";
-// }
-// else if($t<"20"){
-//     echo "Have a good day";
-// }
-// else{
-//     echo "Have a good night";
-// }
-
-// echo "<br>";
-// $cars =array("volvo","Ferrari","BMW");
-// echo "I like". $cars[0]. ",". $cars[1] ."and" .$cars[2];
-// echo "<br>";
-// $arrlength= count($cars);
-
-
-
-// for($x=0;$x<$arrlength; $x++){
-//     echo $cars[$x];
-//     echo "<br>";
-// }
-
-// $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
-
-// foreach($age as $x => $x_value) {
-//     echo $x . "=" . $x_value;
-//     echo "<br>";
-// }
-
-
+}
 
 ?>
+</body>
+</html>
+
